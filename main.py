558e2fa4e77ea0cf868d27f1932052c7e2b6ba2d
Ben Konrad Meyer <ben.k.meyer@gmail.com>
2021-06-29 12:05:23 +0200

    adjust data import

--- main.py
+++ main.py
@@ -1,63 +1,80 @@
 #!/usr/bin/env python
 # PYTHON_ARGCOMPLETE_OK
-
+import argparse
 import sys
+import os
 
 import numpy as np
 import pandas as pd
-
-import os
-import yaml
-import glob
-import hashlib
-import uuid
-
-from typing import Optional
-import matplotlib.pyplot as plt
-
+import argcomplete
+import tensorflow as tf
 
 from argparse import ArgumentParser
-argcomplete_available = False
 
-try:
-    import argcomplete
-    argcomplete_available = True
-except ModuleNotFoundError:
-    pass
+from numpy import NaN
 
 
-parser = ArgumentParser("Uses tensorflow to predict a single value")
-parser.add_argument('-i', '--input-file', type=str,
-                    help='the input csv file to read and predict from')
-parser.add_argument('-p', '--permanency-folder', type=str,
-                    help='the folder to use for permanency saving model data', default='permanency')
-parser.add_argument('-t', '--training-file', type=str,
-                    help='the csv file to train the model. Leave empty to force model reuse')
-parser.add_argument('-f', '--force-retrain', action='store_true',
-                    help='force the program to retrain the model,'
-                         ' even if there is an applicable model in the permanency folder')
-parser.add_argument('-o', '--out-file', type=str,
-                    help='the file to write the predictions to. '
-                         'Has no effect if -i is not specified', default='results.csv')
-if argcomplete_available:
+def main():
+    print(sys.version)
+    args = perform_argparse()
+    model = get_model(args)
+    if args.training_file is not None:
+        train_model(model, args)
+    if args.input_file is not None:
+        parse_input(model, args)
+
+
+def perform_argparse() -> argparse.Namespace:
+    parser = ArgumentParser("Uses tensorflow to predict a single value")
+    parser.add_argument('-i', '--input-file', type=str,
+                        help='the input csv file to read and predict from')
+    parser.add_argument('-p', '--permanency-folder', type=str,
+                        help='the folder to use for permanency saving model data', default='permanency')
+    parser.add_argument('-t', '--training-file', type=str,
+                        help='the csv file to train the model. Leave empty to force model reuse')
+    parser.add_argument('-f', '--force-retrain', action='store_true',
+                        help='force the program to retrain the model,'
+                             ' even if there is an applicable model in the permanency folder')
+    parser.add_argument('-o', '--out-file', type=str,
+                        help='the file to write the predictions to. '
+                             'Has no effect if -i is not specified', default=sys.stdout)
     argcomplete.autocomplete(parser)
-args = parser.parse_args()
+    args = parser.parse_args()
 
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.layers.experimental import preprocessing
+    if args.input_file == '-':
+        args.input_file = sys.stdin
+    return args
 
-print(sys.version)
-print(tf.__version__)
 
-if not args.force_retrain and os.path.isdir(os.path.join(args.permanency_folder, 'model')):
-    model = tf.keras.models.load_model(os.path.join(args.permanency_folder, 'model'))
-else:
-    if args.training_file is None:
-        sys.exit("No available model and no training file - one of these must be available")
+def get_model(args):
+    if not args.force_retrain and os.path.isdir(os.path.join(args.permanency_folder, 'model')):
+        model = tf.keras.models.load_model(os.path.join(args.permanency_folder, 'model'))
+    else:
+        training_dataset = read_file(args.training_file)
+        train_dataset = training_dataset.sample(frac=0.8, random_state=0)
+        test_dataset = training_dataset.drop(train_dataset.index)
 
-    training_dataset = pd.read_csv(args.training_file)
+        train_features = train_dataset
+        test_features = test_dataset
+
+        normalizer = tf.keras.layers.experimental.preprocessing.Normalization()
+        normalizer.adapt(np.array(train_features))
+
+        model = tf.keras.Sequential([
+            normalizer,
+            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dense(units=1)
+        ])
+
+        model.compile(
+            optimizer=tf.optimizers.Adam(learning_rate=0.001),
+            loss='mean_absolute_error')
+    return model
+
+
+def train_model(model, args):
+    training_dataset = read_file(args.training_file)
     train_dataset = training_dataset.sample(frac=0.8, random_state=0)
     test_dataset = training_dataset.drop(train_dataset.index)
 
@@ -66,26 +83,64 @@ else:
 
     train_labels = train_features.pop('Result')
     test_labels = test_features.pop('Result')
-
-    normalizer = preprocessing.Normalization()
-    normalizer.adapt(np.array(train_features))
-
-    model = tf.keras.Sequential([
-        normalizer,
-        layers.Dense(64, activation='relu'),
-        layers.Dense(64, activation='relu'),
-        layers.Dense(units=1)
-    ])
-
-    model.compile(
-        optimizer=tf.optimizers.Adam(learning_rate=0.001),
-        loss='mean_absolute_error')
+    print('Start training')
     model.fit(train_features, train_labels, epochs=10)
+    print('Finish training')
     model.save(os.path.join(args.permanency_folder, 'model'))
 
-if args.input_file is not None:
-    input_data = pd.read_csv(args.input_file)
-    predictions = model.predict(input_data).flatten()
-    with open(args.out_file, 'w') as output_file:
-        output_file.write(predictions)
 
+def parse_input(model, args):
+    input_data = read_file(args.input_file)
+    predictions = model.predict(input_data).flatten()
+    input_data['Result'] = predictions
+    input_data.to_csv(args.out_file, index=False)
+
+
+def read_file(filename):
+    dtypes = {'crop': 'string', 'Timing': 'string', 'TDS': 'string', 'TSCF': 'string', 'Scenario': 'string', 'DT50': 'float64', 'KOC': 'float64', 'Freundlich': 'float64', 'Result': 'float64', 'Day': 'float64', 'Month': 'float64'}
+    dataframe = pd.read_csv(filename, dtype=dtypes, na_values=['Na'])
+    dataframe.drop(dataframe[dataframe.Result == NaN].index, inplace=True)
+    dataframe.crop = pd.to_numeric(dataframe.crop.astype('category'), errors='coerce')
+    dataframe.Timing = pd.to_numeric(dataframe.Timing.astype('category'), errors='coerce')
+    dataframe.TDS = pd.to_numeric(dataframe.TDS.astype('category'), errors='coerce')
+    dataframe.TSCF = pd.to_numeric(dataframe.TSCF.astype('category'), errors='coerce')
+    dataframe.Scenario = pd.to_numeric(dataframe.Scenario.astype('category'), errors='coerce')
+    crops = pd.Series([
+        'apples',
+        'maize',
+        'veggy',
+    ], dtype='category')
+    timings = pd.Series([
+        'early',
+        'mid',
+        'late',
+    ], dtype='category')
+    tds = pd.Series([
+        'off',
+        'on',
+    ], dtype='category')
+    tscf = pd.Series([
+        'off',
+        'on',
+    ], dtype='category')
+    scenarios = pd.Series([
+        'Chat',
+        'Hamb',
+        'Joki',
+        'Krems',
+        'Okeh',
+        'Piac',
+        'Port',
+        'Sevi',
+        'Thiv',
+    ], dtype='category')
+    # dataframe['crop'] = dataframe['crop'].apply(lambda x: x if x is int or x is float else crops.index(x))
+    # dataframe['Timing'] = dataframe['Timing'].apply(lambda x: x if x is int or x is float else timings.index(x))
+    # dataframe['TDS'] = dataframe['TDS'].apply(lambda x: x if x is int or x is float else tds.index(x))
+    # dataframe['TSCF'] = dataframe['TSCF'].apply(lambda x: x if x is int or x is float else tscf.index(x))
+    # dataframe['Scenario'] = dataframe['Scenario'].apply(lambda x: x if x is int or x is float else scenarios.index(x))
+    return dataframe
+
+
+if __name__ == '__main__':
+    main()
